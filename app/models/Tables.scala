115704cef9e6657f84ca06145eb64b5e7a84a3e0
kaznishi <kaz.nishimu@gmail.com>
2016-06-25 22:46:40 +0900

    動かなかったのでいったんコメントアウト

--- app/models/Tables.scala
+++ app/models/Tables.scala
@@ -1,10 +1,10 @@
+/*
 package models
 // AUTO-GENERATED Slick data model
 /** Stand-alone Slick data model for immediate use */
 object Tables extends {
   val profile = slick.driver.H2Driver
 } with Tables
-
 /** Slick data model trait for extension, choice of backend or usage in the cake pattern. (Make sure to initialize this late.) */
 trait Tables {
   val profile: slick.driver.JdbcProfile
@@ -74,3 +74,4 @@ trait Tables {
   /** Collection-like TableQuery object for table Users */
   lazy val DeliveryLogs = new TableQuery(tag => new DeliveryLogs(tag))
 }
+*/
