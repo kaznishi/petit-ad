50db7de1f3d2a7042e725bd793c17609db17fce5
kaznishi <kaz.nishimu@gmail.com>
2016-06-27 13:30:13 +0900

    evolutionの内容を変更

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,16 +1,23 @@
-# Users スキーマ
+# campaigns,delivery_logs schema
 
 # --- !Ups
 
-CREATE TABLE User (
+CREATE TABLE campaigns (
+    id integer(10) NOT NULL AUTO_INCREMENT,
+    title varchar(255) NOT NULL,
+    content varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE delivery_logs (
     id bigint(20) NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    fullname varchar(255) NOT NULL,
-    isAdmin boolean NOT NULL,
+    campaign_id integer(10) NOT NULL,
+    created_at datetime NOT NULL,
     PRIMARY KEY (id)
 );
 
 # --- !Downs
 
-DROP TABLE User;
+DROP TABLE campaigns;
+DROP TABLE delivery_logs;
+
---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,10 @@
+# initial data insertion
+
+# --- !Ups
+
+INSERT INTO campaigns VALUES (1,'test campaign 1','広告の内容1'),(2, 'test campaign 2','広告の内容2');
+
+INSERT INTO delivery_logs VALUES (1,1,'2016-06-25 12:00:00'),(2,1,'2016-06-25 12:00:05');
+
+# --- !Downs
+
