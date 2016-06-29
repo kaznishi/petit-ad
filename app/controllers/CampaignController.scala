672ac8ec408db390afe07c96493ad75fa30a767f
kaznishi <kaz.nishimu@gmail.com>
2016-06-29 21:55:13 +0900

    addのform投稿を実装中。が、うまく動かない。

--- app/controllers/CampaignController.scala
+++ app/controllers/CampaignController.scala
@@ -1,20 +1,50 @@
 package controllers
 
 import models._
+import play.api.data._
+import play.api.data.Forms._
+
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits._
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
+
+object CampaignController {
+
+  case class CampaignForm(id: Option[Int], title: String, content: String)
+
+  val campaignForm = Form(
+    mapping(
+      "id" -> optional(number),
+      "title" -> nonEmptyText,
+      "content" -> nonEmptyText
+    )(CampaignForm.apply)(CampaignForm.unapply)
+  )
+}
 
 class CampaignController extends Controller {
+  import CampaignController._
 
   def list = Action.async {
     CampaignsDAO.findAll.map { records => Ok(views.html.Campaign.List(records)) }
   }
 
-  def add = Action {
+  def addView = Action {
     Ok(views.html.Campaign.Add())
+  }
+
+  def addPost = Action.async { implicit rs =>
+    campaignForm.bindFromRequest.fold(
+      error => {
+        CampaignsDAO.findById(1).map { _ =>
+          BadRequest(views.html.Campaign.Add())
+        }
+      },
+      form => {
+        CampaignsDAO.findById(1).map { _ =>
+          Redirect(routes.CampaignController.list)
+        }
+      }
+    )
   }
 
   def edit = Action {
