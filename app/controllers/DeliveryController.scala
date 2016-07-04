bae3abe3d4b8322470ee746aedfbd9dad71d26a8
kaznishi <kaz.nishimu@gmail.com>
2016-07-04 20:05:46 +0900

    書き方変えて何パターンか同じ処理を実装

--- app/controllers/DeliveryController.scala
+++ app/controllers/DeliveryController.scala
@@ -3,7 +3,7 @@ package controllers
 import models._
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-
+import scala.concurrent.Future
 import scala.util.Random
 import org.joda.time.DateTime
 import play.api.libs.json._
@@ -57,4 +57,57 @@ class DeliveryController extends Controller {
       }
     }
   }
+
+  /**
+    * DeliveryLogsのinsertをmapを使って同期処理完了後に画面遷移するように変更してみたもの
+    * @return
+    */
+  def random2 = Action.async { implicit rs =>
+    CampaignsDAO.getCampaignIds.flatMap {
+      case ids => {
+        val r = new Random
+        val index = r.nextInt().abs % ids.length
+
+        CampaignsDAO.findById(ids(index)).flatMap {
+          case Some(record) => {
+            DeliveryLogsDAO.insert(DeliveryLog(None, record.id.get, new DateTime(), new DateTime())).map { r =>
+              Ok(Json.obj("data" -> Delivery(record)))
+            }
+          }
+          case None =>
+            Future.successful(Ok(Json.obj("data" -> Delivery("no title", "no content"))))
+
+        }
+      }
+    }
+
+  }
+
+  /**
+    * random2をforを使って書き直したもの
+    * @return
+    */
+  def random3 = Action.async { implicit rs =>
+    for {
+      ids <- CampaignsDAO.getCampaignIds
+      optRecord <- {
+        val r = new Random
+        val index = r.nextInt().abs % ids.length
+
+        CampaignsDAO.findById(ids(index))
+      }
+      r <- {
+        optRecord match {
+          case Some(record) => DeliveryLogsDAO.insert(DeliveryLog(None, record.id.get, new DateTime(), new DateTime()))
+          case None => Future.successful(None)
+        }
+      }
+    } yield {
+      optRecord match {
+        case Some(record) => Ok(Json.obj ("data" -> Delivery (record) ) )
+        case None => Ok(Json.obj("data" -> Delivery("no title", "no content")))
+      }
+    }
+  }
+
 }
